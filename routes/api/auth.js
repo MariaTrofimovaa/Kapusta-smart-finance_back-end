b77469042359e3b8708652d47d524722deeb0015
MariaTrofimovaa <73877805+MariaTrofimovaa@users.noreply.github.com>
2021-09-16 20:58:51 +0300

    added server req

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -10,7 +10,7 @@ const { auth: ctrl } = require("../../controllers");
 const router = express.Router();
 
 router.post("/signup", validation(joiSchema), ctrl.signup);
-router.get("./verify/:verifyToken", ctrl.verifyEmail); //запрос на считывание токена, при переходе юзера по ссылке в письме
+router.get("/verify/:verifyToken", ctrl.verifyEmail); //запрос на считывание токена, при переходе юзера по ссылке в письме
 router.post("/signin", validation(joiSchema), ctrl.signin);
 // router.get("/current", authentificate, ctrl.getCurrentUser);
 // router.get("/logout", authentificate, ctrl.logout);
