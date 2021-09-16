2248671511bcfa1431dd9797ffecf1556b70a1fc
Svitlana Burba <Svitlana.Burba@gmail.com>
2021-09-16 17:39:18 +0200

    added support for authorization for Balance

--- controllers/user/getBalance.js
+++ controllers/user/getBalance.js
@@ -2,7 +2,7 @@ const { users: service } = require("../../services");
 
 const getBalance = async (req, res, next) => {
   try {
-    const {balance} = await service.getById('61422103e1a984521715ca6c');
+    const balance = req.user.balance; // данные пользователя из базы получает предыдуший authentificate, так что еще один запрос в базу уже не нужен
 
     res.status(201).json({
       status: "success",
