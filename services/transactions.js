6790c577ecfd5e041a12b85b2244f010fabed0f9
MariaTrofimovaa <73877805+MariaTrofimovaa@users.noreply.github.com>
2021-09-22 16:38:39 +0300

    fixes getForMonth fn

--- services/transactions.js
+++ services/transactions.js
@@ -55,7 +55,7 @@ const getForMonth = async (id, type, month) => {
   const data = await Transaction.find({ userId: id, transactionType: type });
 
   const filtered = data.filter((obj) => {
-    const monthSLice = obj.date.slice(0, 7);
+    const monthSLice = obj.date.slice(3);
     return monthSLice === month;
   });
 
