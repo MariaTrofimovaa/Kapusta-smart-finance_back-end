60102ead58879030be30d243f3263b4ad748b4a3
Alona-Mykhaylenko <79852771+Alona-Mykhaylenko@users.noreply.github.com>
2021-09-22 09:41:46 +0200

    not working categories request

--- services/categories.js
+++ services/categories.js
@@ -1,19 +1,29 @@
+// const { Category } = require("../models");
+
+// const listExpenseCategories = () => {
+//   return Category.find(
+//     { transactionType: "expense" },
+//     "_id date description amount category transactionType"
+//   );
+// };
+// const listIncomeCategories = () => {
+//   return Category.find(
+//     { transactionType: "income" },
+//     "_id date description amount category transactionType"
+//   );
+// };
+
+// module.exports = {
+//   listExpenseCategories,
+//   listIncomeCategories,
+// };
+
 const { Category } = require("../models");
 
-const listExpenseCategories = () => {
-  return Category.find(
-    { transactionType: "expense" },
-    "_id date description amount category transactionType"
-  );
-};
-const listIncomeCategories = () => {
-  return Category.find(
-    { transactionType: "income" },
-    "_id date description amount category transactionType"
-  );
+const getByType = (type) => {
+  return Category.find({ transactionType: type });
 };
 
 module.exports = {
-  listExpenseCategories,
-  listIncomeCategories,
+  getByType,
 };
