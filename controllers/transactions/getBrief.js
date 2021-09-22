764364b6bc9809208ac7e5d07e41218962c40886
TetyanaRed <redcars2012@gmail.com>
2021-09-22 17:44:28 +0300

    test

--- controllers/transactions/getBrief.js
+++ controllers/transactions/getBrief.js
@@ -1,13 +1,14 @@
 const { transactions: service } = require("../../services");
 
 const getBrief = async (req, res, next) => {
+  console.log(req.query);
   try {
     // const { _id } = req.user;
     // console.log(_id);
     // console.log(req);
     // const allIncomes = await service.readBrief(_id, year);
     const allIncomes = await service.readBrief(req.query);
-
+    console.log(allIncomes);
     return res.status(201).json({
       status: "success",
       code: 201,
