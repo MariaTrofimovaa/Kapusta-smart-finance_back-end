d67ae022ac285eb2e48b677d1968ce5f2f84bcf8
yamchinsky <66372922+yamchinsky@users.noreply.github.com>
2021-09-27 08:55:42 +0300

    wrappers added

--- controllers/auth/verifyEmail.js
+++ controllers/auth/verifyEmail.js
@@ -1,25 +1,21 @@
 const { users: service } = require("../../services");
 
 const verifyEmail = async (req, res, next) => {
-  try {
-    const { verifyToken } = req.params;
-    const user = await service.getOne({ verifyToken });
-    if (!user) {
-      return res.status(404).json({
-        status: "error",
-        code: 404,
-        message: "user Not found by token during email verification",
-      });
-    }
-    await service.update(user._id, { verify: true, verifyToken: null });
-    res.json({
-      status: "success",
-      code: 200,
-      message: "Verification success",
+  const { verifyToken } = req.params;
+  const user = await service.getOne({ verifyToken });
+  if (!user) {
+    return res.status(404).json({
+      status: "error",
+      code: 404,
+      message: "user Not found by token during email verification",
     });
-  } catch (error) {
-    next(error);
   }
+  await service.update(user._id, { verify: true, verifyToken: null });
+  res.json({
+    status: "success",
+    code: 200,
+    message: "Verification success",
+  });
 };
 
 module.exports = verifyEmail;
